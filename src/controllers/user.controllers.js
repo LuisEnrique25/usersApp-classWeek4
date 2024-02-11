810045fdba02fc96ab3f21b8013c73e80b34d87e
LuisEnrique25 <luisrg2225@gmail.com>
2024-02-11 17:53:20 -0600

    proyecto users

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,83 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const getAll = catchError(async(req, res) => {
+    const results = await User.findAll();
+    return res.json(results);
+});
+
+const create = catchError(async(req, res) => {
+
+    
+    const { password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const body = {...req.body, password: hashedPassword};
+    const result = await User.create(body);
+    return res.status(201).json(result);
+});
+
+const getOne = catchError(async(req, res) => {
+    const { id } = req.params;
+    const result = await User.findByPk(id);
+    if(!result) return res.sendStatus(404);
+    return res.json(result);
+});
+
+const remove = catchError(async(req, res) => {
+    const { id } = req.params;
+    const result = await User.destroy({ where: {id} });
+    if(!result) return res.sendStatus(404);
+    return res.sendStatus(204);
+});
+
+const update = catchError(async(req, res) => {
+    const { id } = req.params;
+
+    delete req.body.password;
+    delete req.body.email;
+    const result = await User.update(
+        req.body,
+        { where: {id}, returning: true }
+    );
+    if(result[0] === 0) return res.sendStatus(404);
+    return res.json(result[1][0]);
+});
+
+const login = catchError(async(req, res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({where: {email}});
+    if(!user) return res.status(404).json({error:'Invalid Credentials'});
+
+    //password validation
+
+    const isValid = await bcrypt.compare(password, user.password); //regresa true o false
+    if(!isValid) return res.status(404).json({error:'Invalid Credentials'});
+
+    //generacion del token 
+    const token = jwt.sign(
+        { user }, 
+        process.env.TOKEN_SECRET, //cerradura que tenemos en .env
+        { expiresIn: '1d' }
+    )
+
+    return res.json({user, token})
+});
+
+const logged = catchError(async(req, res) =>{
+    const user = req.user;
+    return res.json(user);
+});
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    remove,
+    update,
+    login, 
+    logged
+}
